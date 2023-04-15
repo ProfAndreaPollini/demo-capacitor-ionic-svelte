b34a328754fc0a1a701333811698c082f89c0dd4
Andrea Pollini <prof.andrea.pollini@gmail.com>
2023-04-15 17:24:35 +0200

    demo first import

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'demo-capacitor-ionic-svelte',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
